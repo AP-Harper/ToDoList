b36a6adc407a74907c0c70fd253f74ad461b89cf
Andrew Harper <andrewpaulharper@outlook.com>
2024-08-12 10:50:27 +0100

    Refactored ToDo List RequestMapping

--- src/main/java/com/apharper/todolist/Controllers/ToDoListController.java
+++ src/main/java/com/apharper/todolist/Controllers/ToDoListController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/todos")
+@RequestMapping({"tasks", "tasks/"})
 public class ToDoListController {
     private final ToDoListService toDoListService;
 
